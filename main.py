68cf45caf93b77fb8630426ba33cf78bb7c13e13
Yaromur0605 <yarko.mur@gmail.com>
2023-03-11 18:06:49 +0200

    hghghgh

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+class cat():
+    def __init__(self, name, chill):
+        self.chill = chill
+        self.name = name
+    def sleep(self):
+        print("cat sleep")
+        self.chill += 1
+
+    def jump(self):
+        print("cat jumping")
+        self.chill -= 0.5
+    def chill(self):
+        print("cat chill")
+        self.chill =+ 1
+
+
